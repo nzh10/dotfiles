e49e68bb960dec5721262c24875ccbcb1d154ef6
nZh10 <coding@nzh10.com>
2023-07-05 17:27:48 +0800

    update installation script in win and z.lua

--- installation_scripts/install_windows.ps1
+++ installation_scripts/install_windows.ps1
@@ -4,12 +4,15 @@ if (!$IsWindows || $PSVersionTable.Version -le [System.Version]"6.0") {
 }
 $current_path = Split-Path -Path $MyInvocation.MyCommand.Source
 
-if (! $current_path -eq "$HOME/.config/dotfiles") {
-    Write-Error "This scripts has hardcoded the installation path."
+$check_path = Join-Path ((Get-Item $current_path).parent.FullName) "" -Resolve
+$hardcoded_path = Join-Path "$HOME/.config/dotfiles" '' -Resolve
+if (!($check_path -eq $hardcoded_path)) {
+    Write-Host "$check_path vs. $hardcoded_path"
+    Write-Error "This scripts has hardcoded the installation path, current path is $check_path"
     exit
 }
 
-if (! Get-Package -Name "posh-git") {
+if (!(Get-Package -Name 'posh-git')) {
     PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 }
 
@@ -35,10 +38,10 @@ if (!
 }
 
 $profile_name = "Microsoft.PowerShell_profile.ps1"
-$link_path = "$env:USERPROFILE/Documents/Powershell/$profile_name"
+$link_path = "$([Environment]::GetFolderPath('MyDocuments'))/Powershell/$profile_name"
 $starship_path = "$env:USERPROFILE/.config/starship.toml"
 
-Remove-Item $link_path,$starship_path
+Remove-Item $link_path,$starship_path -ErrorAction SilentlyContinue
 New-Item -ItemType SymbolicLink -Target "$(Split-Path -Path $MyInvocation.MyCommand.Source)/../$profile_name" -Path $link_path
 New-Item -ItemType SymbolicLink -Target "$(Split-Path -Path $MyInvocation.MyCommand.Source)/../starship.toml" -Path $starship_path
 
